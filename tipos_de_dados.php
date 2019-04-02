28c57a3cb0d319b09154efd2fe395915b1aed8f3
Dirceu Junio <dirceujunio2525@gmail.com>
2019-04-02 14:00:50 -0300

    Editado

--- tipos_de_dados.php
+++ tipos_de_dados.php
@@ -1,4 +1,31 @@
 <?php
 
+//Tipos básicos:
+
+$nome ="Shady";//String
+$site = 'https://github.com/AkaShady';//String
+
+$ano = 1999;//Integer
+$salario =  10000.99;//Float
+$bloqueado = false;//Boolean
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Tipos composto:
+
+$idiomas = array ("ingles", "frances", "russo");//Arry
+//echo $idiomas[2];
+
+$nascimento = new DateTime();//Object
+//var_dump($nascimento);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Tipos especiais:
+
+$arquivo = fopen("tipos_de_dados.php", "r");//Resource
+//var_dump($arquivo);
+
+$nulo  = NULL;//NULL
 
 ?>
